d0206b995b9ff26e50e3cca98fe126cdf8f6a6a7
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2025-04-11 17:17:03 +0100

    init

---
+++ FloatingButton/FloatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonApp.swift
+//  FloatingButton
+//
+//  Created by Kod Enigma on 11/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
